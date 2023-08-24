8241563a2f55356e1d97c144ea7369ccf7d39853
Thabisile <thabisilemkhize@gmail.com>
2023-08-24 14:58:31 -0700

    Nop

---
+++ file7.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ *f_nop - doesn’t do anything
+ *@start: start of the stack
+ *@index: an integer
+ */
+void f_nop(stack_t **start, unsigned int index)
+{
+	(void) index;
+	(void) start:
+}
