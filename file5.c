9ccb39886b729b65db517b25636080c5d7ef9810
Thabisile <thabisilemkhize@gmail.com>
2023-08-24 14:44:19 -0700

    Pint

---
+++ file5.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * f_pint - prints the value at the top of the stack
+ *@start: start of the stack
+ *@index: an integer
+ */
+{
+	if (*start == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", index);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*start);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*start)->n);
+}
