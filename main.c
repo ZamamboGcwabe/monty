26edb47a58f9b6b6bb8b2ee0ba24170a64fdfbaf
Zamambo <122832552+ZamamboGcwabe@users.noreply.github.com>
2024-05-17 14:10:20 +0100

    main.c

--- main.c
+++ main.c
@@ -1,29 +1,27 @@
 #include "monty.h"
-bus_t bus = {NULL, NULL, NULL};
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
- *main - interpretes the monty code
- *@argv: argument vector
- *argc: arument count
- *Return: 0 on success
- */
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
 int main(int argc, char *argv[])
 {
-	unsigned int index = 0;
-	FILE *file;
-	stack_t *stack = NULL;
-	ssize_t read_line = 1;
-	size_t size = 0;
 	char *content;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
 	file = fopen(argv[1], "r");
 	bus.file = file;
-
 	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
@@ -34,14 +32,14 @@ int main(int argc, char *argv[])
 		content = NULL;
 		read_line = getline(&content, &size, file);
 		bus.content = content;
-		index++;
-
+		counter++;
 		if (read_line > 0)
 		{
-			execle(content, &stack, index, file);
+			execute(content, &stack, counter, file);
 		}
 		free(content);
 	}
+	free_stack(stack);
 	fclose(file);
-	return (0);
+return (0);
 }
